763ca4c31a237aeb338533cd110364d95e4df0c6
Catalin-Cbn <catalincbn@gmail.com>
2025-02-10 16:39:03 +0100

    Application Boot e inizio CliManager

--- demo6/src/main/java/org/jaita138/esercitazione/demo6/Demo6Application.java
+++ demo6/src/main/java/org/jaita138/esercitazione/demo6/Demo6Application.java
@@ -1,5 +1,7 @@
 package org.jaita138.esercitazione.demo6;
 
+import org.jaita138.esercitazione.demo6.service.AutoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,13 +9,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo6Application implements CommandLineRunner {
 
+	@Autowired
+	private AutoreService autoreService;
+
+	@Autowired
+	private GenereService genereService;
+
+	@Autowired
+	private LibroService libroService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Demo6Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello group 3!");
+		
+		new CliManager(autoreService, genereService, libroService);
 	}
 
 }
--- demo6/src/main/java/org/jaita138/esercitazione/demo6/cli/CliManager.java
+++ demo6/src/main/java/org/jaita138/esercitazione/demo6/cli/CliManager.java
@@ -1,5 +1,20 @@
 package org.jaita138.esercitazione.demo6.cli;
 
+import org.jaita138.esercitazione.demo6.service.AutoreService;
+
 public class CliManager {
 
+    private Scanner scanner;
+
+    private AutoreService autoreService;
+    private LibroService libroService;
+    private GenereService genereService;
+
+    public CliManager(
+        AutoreService autoreService,
+        LibroService libroService,
+        GenereService genereService) {
+            
+        }
+    )
 }
